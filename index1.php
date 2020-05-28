f45987ed15bcc07d80e26e8fd246bb6c54f7cda7
Luiz <designarttdev@gmail.com>
2020-05-28 12:21:04 -0300

    Corrigido problema de session

--- index1.php
+++ index1.php
@@ -6,36 +6,33 @@ include 'scripts/password.php';
 $usuario = $_POST['usuario'];
 $senhaPost = $_POST['senha'];
 
-$sql = "SELECT mail_usuario FROM usuarios WHERE mail_usuario = '$usuario' and status_usuario = 'Ativo'";
+$sql = "SELECT mail_usuario, senha_usuario FROM usuarios WHERE mail_usuario = '$usuario' and status_usuario = 'Ativo'";
 $buscar = mysqli_query($conexao, $sql);
 
 $total = mysqli_num_rows($buscar);
 
-while($array = mysqli_fetch_array($buscar)){
+if ($total > 0) {
+    while ($array = mysqli_fetch_array($buscar)) {
 
-    $senhaBanco = $array['senha_usuario'];
+        $senhaBanco = $array['senha_usuario'];
 
-    $senhadecodificada = sha1($senhaPost);
+        $senhadecodificada = sha1($senhaPost);
 
-    if ($total > 0) {
-        
+
         if ($senhadecodificada == $senhaBanco) {
-        
+
             session_start();
             $_SESSION['usuario'] = $usuario;
 
             header("Location: menu.php");
-            
+
         } else {
-            header("Location: erro_teste.php");    
+            header("Location: erro.php");
         }
-
-
-    } else {
-        header("Location: erro.php");
     }
 
-
+} else {
+    header("Location: erro.php");
 }
 
 ?>
